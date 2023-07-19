a9e7dfa7f4c98b4fdeb83872487d2c32e89a3df8
GABRIEL SANT ANA DA SILVA <gabriel.sasilva4@senacsp.edu.br>
2023-07-19 15:29:24 -0300

    first commit

---
+++ WFGravarDadosMySQL/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace WFGravarDadosMySQL
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection Conexao;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data_source = "datasource=localhost;username=root;password=;database=db_agenda";
+                Conexao = new MySqlConnection(data_source);
+
+                string sql = "INSERT INTO contato (nome,email,telefone) VALUES" +
+                    "('" + txtNome.Text + "','" + txtEmail.Text + "','" + txtTelefone.Text + "')";
+
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+                Conexao.Open();
+                comando.ExecuteReader();
+                MessageBox.Show("Você foi cadastrado");
+            }catch (Exception ex  ) 
+             {
+                MessageBox.Show(ex.Message);
+             }
+            finally
+            {
+                Conexao.Close();
+            }
+
+                    
+            }
+        }
+
+        
+    }
+
